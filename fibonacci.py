8db125f775394d0c18ed99e8e7da05e0169f811c
Anamika Singh Raj <anamikasinghrajraj@gmail.com>
2025-07-17 04:53:11 -0700

    Update fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+a = 0
+b = 1
+num = int(input("Enter how many Fibonacci numbers to print: "))
+
+if num == 1:
+    print(a)
+else:
+    print(a)
+    print(b)
+    for i in range(2, num):
+        c = a + b
+        print(c)
+        a = b
+        b = c
