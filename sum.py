7f39379f164549fe1c2b15b0bd84212e3779b950
Anamika Singh Raj <anamikasinghrajraj@gmail.com>
2025-07-18 04:21:07 -1200

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,4 @@
+a = 23.4
+b = 34
+c = a + b
+print("The sum of a and b is", c)
---
+++ temp.py
@@ -0,0 +1,6 @@
+# fahrenheit = (celsius * 9/5) + 32
+# celsius = (fahrenheit - 32) * 5/9
+temp1 = float(input("Enter temperature in Celsius: "))
+temp2 = (temp1 * 9/5) + 32
+print(temp2)
+
