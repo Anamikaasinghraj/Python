2af889dd2cb58df0141d982e543faf51bd25059a
Anamika Singh Raj <anamikasinghrajraj@gmail.com>
2025-09-08 22:19:49 +0530

    Add files via upload

---
+++ cylinder.py
@@ -0,0 +1,5 @@
+# volume of cylinder = π * r^2 * h
+r = int(input("enter radius: "))
+h = int(input("enter height: "))
+volume = 3.14 * (r ** 2) * h
+print(volume)
---
+++ movie.py
@@ -0,0 +1,5 @@
+a = input("enter a mvie name:")
+b = input("enter a movie name again")
+c = input("enter a movie name again")
+list = [a, b, c]
+print(list)
